3e2e8e3ce93b1d781e9f009b74f7b72684a1dc3d
Matthew Garcia <matt.e.garcia@gmail.com>
2021-07-06 12:21:50 -0500

    new Clock class/module

---
+++ Clock.py
@@ -0,0 +1,34 @@
+# pylint: disable=C0103
+"""
+Python script "Clock.py"
+by Matthew Garcia, Postdoctoral Research Associate
+Dept. of Forest and Wildlife Ecology
+University of Wisconsin - Madison
+matt.e.garcia@gmail.com
+
+Copyright (C) 2021 by Matthew Garcia
+"""
+
+
+from datetime import timedelta
+
+
+class Clock():
+    """Simulation clock in UTC."""
+
+    def __init__(self, sim):
+        self.start_dt = sim.start_time
+        self.start_dt_str = self.start_dt.isoformat()
+        self.end_dt = sim.end_time
+        self.start_dt_str = self.start_dt.isoformat()
+        self.dt_interval = sim.dt
+        self.current_dt = sim.start_time
+        self.current_dt_str = self.current_dt.isoformat()
+
+    def advance_clock(self):
+        """Advance simulation time by dt_interval, in seconds."""
+        print('%s : advancing clock, dt = %d seconds' % (self.current_dt_str, self.dt_interval))
+        self.current_dt += timedelta(seconds=self.dt_interval)
+        self.current_dt_str = self.current_dt.isoformat()
+
+# end Clock.py
